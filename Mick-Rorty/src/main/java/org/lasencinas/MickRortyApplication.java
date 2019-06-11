58d97b56056964f5cab0a92c9c5ed03c1afef242
marc monjon <sysmarc13@hotmail.com>
2019-06-11 16:40:08 +0200

    Proyecto iniciado

---
+++ Mick-Rorty/src/main/java/org/lasencinas/MickRortyApplication.java
@@ -0,0 +1,13 @@
+package org.lasencinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MickRortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MickRortyApplication.class, args);
+	}
+
+}
